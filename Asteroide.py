050f0c3f1b3f5dd39e129f2a79637373cfe87f96
JcBlan <blancojulic@gmail.com>
2019-03-27 17:47:05 -0300

    modificados metodos de asteroide y añadido test daño invasor

--- Asteroide.py
+++ Asteroide.py
@@ -6,5 +6,8 @@ class Asteroide(object):
     def __init__(self, vel):
         self.velocidad = vel
     
-    def chocar(self, nave):
+    def chocarNave(self, nave):
         nave.vida -= (self.velocidad * nave.velocidad) / 200
+
+    def chocarInvasor(self, invasor):
+        invasor.vida -= (self.velocidad * invasor.velocidad) / 200
--- testAsteroide.py
+++ testAsteroide.py
@@ -1,28 +1,35 @@
 import unittest
 from Nave import Nave
 from Asteroide import Asteroide
+from Invasor import Invasor
 
 
 class TestAsteroide(unittest.TestCase):
     
-    def test_chocar_nave(self):
+    def test_chocar_Nave(self):
         nave = Nave(100, 50)
         asteroide = Asteroide(40)
-        asteroide.chocar(nave)
+        asteroide.chocarNave(nave)
         self.assertTrue(nave.vida == 90)
 
     def test_chocar_Nave_SinDano(self):
         nave = Nave(100, 50)
         asteroide = Asteroide(0)
-        asteroide.chocar(nave)
+        asteroide.chocarNave(nave)
         self.assertTrue(nave.vida == 100)
     
     
     def test_chocar_Nave_TodaVel(self):
-        nave = Nave(100, 50)
+        nave = Nave(100, 100)
         asteroide = Asteroide(100)
-        asteroide.chocar(nave)
-        self.assertTrue(nave.vida == 75)
+        asteroide.chocarNave(nave)
+        self.assertTrue(nave.vida == 50)
+
+    def test_chocar_Invasor(self):
+        invasor = Invasor(100, 50)
+        asteroide = Asteroide(60)
+        asteroide.chocarInvasor(invasor)
+        self.assertTrue(invasor.vida == 85)
 
 
 if __name__ == "__main__":
